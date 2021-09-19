bf7def09bd6ea21576aa4368471ca6258bf17165
joe <yousof@new-vision.agency>
2021-09-19 16:10:44 +0200

    Update the UI of repo item and handle its responsive

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -5,7 +5,7 @@ import formatDistanceToNow from "date-fns/formatDistanceToNow";
 const RepoCard = ({ item }) => {
   const createdAt = formatDistanceToNow(new Date(item.created_at));
   return (
-    <div className={"col-12 ps-3 d-flex align-items-center"}>
+    <div className={"col-12 ps-3 py-3 d-flex align-items-center repo-item flex-column flex-md-row"}>
       <img
         className={"avatar-image me-3"}
         src={item.owner.avatar_url}
@@ -13,12 +13,12 @@ const RepoCard = ({ item }) => {
       />
       <div className={"d-flex flex-column"}>
         <h4>{item.name}</h4>
-        <h6>{item.description}</h6>
-        <div>
-          <span className={"me-3 px-2 py-1 item-box"}>
+        <h6 className={'repo-description'}>{item.description}</h6>
+        <div className={'flex-column flex-md-row d-md-block d-flex'}>
+          <span className={"me-3 px-2 py-1 item-box mb-md-0 mb-2 "}>
             Stars: {item.stargazers_count}
           </span>
-          <span className={"me-3 px-2 py-1 item-box"}>
+          <span className={"me-3 px-2 py-1 item-box mb-md-0 mb-2"}>
             Issues: {item.open_issues}
           </span>
           <span>{`Submitted ${createdAt} ago by ${item.owner.login}`}</span>
